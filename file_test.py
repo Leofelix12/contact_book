82224d3a317df7c15f94d67313424977759f8bd5
Felix Danso <fdanso41@gmail.com>
2026-09-19 09:45:56 +0000

    file handling

--- file_test.py
+++ file_test.py
@@ -1,13 +1,7 @@
-with open("greeting.txt", "w") as f:
-    f.write("Hello from Felix")
-
-with open("greeting.txt", "r") as f:
-    content = f.read()
-    print(content)
-
 with open("contacts.txt", "w") as f:
     f.write("Felix, 0551234567\n")
     f.write("Ama, 0207654321\n")
+    f.write("Wendy, 0243539956")
     
 with open("contacts.txt", "r") as f:
     content = f.read()
