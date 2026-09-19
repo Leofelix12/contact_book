116399cca82cf7ac01251e26d22b325b0e0fb097
Felix Danso <fdanso41@gmail.com>
2026-09-19 09:14:23 +0000

    writing contacts to files

--- file_test.py
+++ file_test.py
@@ -4,3 +4,7 @@ with open("greeting.txt", "w") as f:
 with open("greeting.txt", "r") as f:
     content = f.read()
     print(content)
+
+with open("contacts.txt", "w") as f:
+    f.write("Felix, 0551234567\n")
+    f.write("Ama, 0207654321\n")
