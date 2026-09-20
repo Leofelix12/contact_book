47259aa080e9fce66ef7c405a40012ddf47d1993
Felix Danso <fdanso41@gmail.com>
2026-09-20 16:31:32 +0000

    Automate file wrting

--- file_test.py
+++ file_test.py
@@ -1,4 +1,4 @@
-contacts = ["Felix, 0551234567","Ama, 0207654321","Wendy, 0243539956", "Naomi, 0501243564" ]
+contacts = ["Felix, 0551234567"," Ama, 0207654321 ","Wendy, 0243539956" , " Naomi, 0501243564" ]
 with open("contacts.txt", "w") as f:
     for contact in contacts:
         f.write(contact)
