76527f2898a8953e758368e1849784c524837e7f
Felix Danso <fdanso41@gmail.com>
2026-09-20 16:46:01 +0000

    Automate file writting

--- file_test.py
+++ file_test.py
@@ -2,7 +2,7 @@ contacts = ["Felix, 0551234567"," Ama, 0207654321 ","Wendy, 0243539956" , " Naom
 
 with open("contacts.txt", "w") as f:
     for contact in contacts:
-        f.write(contact + "\n")
+        f.write(contact.strip() + "\n")
     
     
 with open("contacts.txt", "r") as f:
