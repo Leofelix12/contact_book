97395db6ca41eddd35df6ca2ede507d605667bb0
Felix Danso <fdanso41@gmail.com>
2026-09-20 16:34:51 +0000

    Automate file writing

--- file_test.py
+++ file_test.py
@@ -1,7 +1,8 @@
 contacts = ["Felix, 0551234567"," Ama, 0207654321 ","Wendy, 0243539956" , " Naomi, 0501243564" ]
+
 with open("contacts.txt", "w") as f:
     for contact in contacts:
-        f.write(contact)
+        f.write(contact + "\n")
     
     
 with open("contacts.txt", "r") as f:
