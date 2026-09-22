5e01f883b49f97078610f8fda013bf45c512d7b2
Felix Danso <fdanso41@gmail.com>
2026-09-22 21:45:13 +0000

    builing contact book app

--- contact_book.py
+++ contact_book.py
@@ -1,6 +1,18 @@
-print("1. Add contact")
-print("2. View contacts")
-print("3. Exit")
+running = True
 
-choice = input("Choose an option: ")
-print("You chose:", choice)
+while running:
+    print("1. Add contact")
+    print("2. View contacts")
+    print("3. Exit")
+
+    choice = input("Choose an option: ")
+
+    if choice == "1":
+        print("You want to add contact.")
+    elif choice == "2":
+        print("You want to view contacts.")
+    elif choice == "3":
+        print("Goodbye.")
+        running = False
+    else:
+        print("Invalid choice. Try again.")
