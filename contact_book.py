4f67a3c456597aa65df1e11113a93445e5235dc8
Felix Danso <fdanso41@gmail.com>
2026-09-23 17:28:17 +0000

    Contactbook app

--- contact_book.py
+++ contact_book.py
@@ -1,5 +1,7 @@
 running = True
 
+contacts = []
+
 while running:
     print("1. Add contact")
     print("2. View contacts")
@@ -9,10 +11,19 @@ while running:
 
     if choice == "1":
         print("You want to add contact.")
+        name = input("Enter name: ")
+        phone = input("Enter phone: ")
+        contacts.append(name + "," + phone)
+        print("contact added.")
+        
     elif choice == "2":
         print("You want to view contacts.")
+        print(contacts)
+        
+        
     elif choice == "3":
         print("Goodbye.")
         running = False
+        
     else:
         print("Invalid choice. Try again.")
